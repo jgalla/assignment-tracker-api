02e0e1f8a6587a6f669274952584e38d0baafb8d
jgalla <jessejgallagher@gmail.com>
2019-02-01 14:39:43 -0500

    Remove student scaffold again, add assignment curl scripts

---
+++ curl-scripts/assignments/create-assignment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl "http://localhost:4741/assignments" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "assignments": {
+      "assignment_name": "'"${NAME}"'",
+      "link": "'"${LINK}"'",
+      "due_date": "'"${DATE}"'",
+      "status": "'"${STATUS}"'"
+    }
+  }'
+
+echo
---
+++ curl-scripts/assignments/get-assignments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "http://localhost:4741/assignments" \
+  --include \
+  --request GET \
+
+echo
