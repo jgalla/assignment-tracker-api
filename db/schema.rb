2005f6c0d44c7a74541db87d5040d1942b862dfb
jgalla <jessejgallagher@gmail.com>
2019-02-01 20:31:48 -0500

    Update schema with assignment reference to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_203259) do
+ActiveRecord::Schema.define(version: 2019_02_02_012802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_02_01_203259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "due_date"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 2019_02_01_203259) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "assignments", "users"
   add_foreign_key "examples", "users"
 end
